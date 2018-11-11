c6fc4e2462c517c8ef1abca55bfe6c778d3ed2da
VitalyDorozhkin <dorozhkin.vitaly@gmail.com>
2018-11-11 12:19:16 +0300

    edit first commit

--- ipoteca.rb
+++ ipoteca.rb
@@ -1,15 +1,16 @@
 percent_year = 0.12
 percent_month = percent_year/12
-sum = 1000000
-payment_month = 60000
+sum = gets.to_i
+payment_month = gets.to_i
 month = 0
 if payment_month > sum*percent_month
   while sum > 0
     puts"#{month/12}y. #{month%12}m. | #{sum} | #{sum*percent_month} | #{payment_month} | #{payment_month - sum*percent_month}"
     sum += sum*percent_month - payment_month
+    break if sum < 0
     month += 1
   end
-  puts"#{month-1/12}y. #{month-1%12}m."
+  puts"#{month/12}y. #{month%12}m."
 else
   puts "IMPOSSIBLE"
 end
