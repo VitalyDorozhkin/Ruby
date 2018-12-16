685df2a458c3eaae2ba1e2ccf2d52458309abf87
VitalyDorozhkin <dorozhkin.vitaly@gmail.com>
2018-12-17 00:00:18 +0300

    add seed.rb

--- CourseApp/db/seeds.rb
+++ CourseApp/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Course.create(name:"C#", price: 20.0, online: true)
+Course.create(name:"Programming in Ruby", price: 20.0, online: true)
+Course.create(name:"Python 3", price: 4000.0, online: false )
+Course.create(name:"C++", price: 2000.0, online: false)
+Course.create(name:"HTML", price: 0.0, online: true)
+Course.create(name:"Js", price: 4990.0, online: true)
+Course.create(name:"Basic", price: 200.0, online: false)
+Course.create(name:"English", price: 0.0, online: false)
